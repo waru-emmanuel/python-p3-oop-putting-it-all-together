5d998b0042795230454c6f36b086874008cbd5c6
EMMANUEL <waruterewachira7@gmail.com>
2024-07-04 01:46:50 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+
+    def __init__(self, title, page_count):
+        '''has the title and page_count passed into __init__, and values can be set to new instance.'''
+        self.title = title
+        self.page_count = page_count
+        
+
+    #property and setter
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        if type(new_title) is str:
+            self._title = new_title
+        else:
+            print("title must be a string")
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if type(new_page_count) is int:
+            self._page_count = new_page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
+        print("Flipping the page...wow, you read fast!")
+
+
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        '''has the brand and size passed to __init__, and values can be set to new instance.'''
+        self.brand = brand
+        self.size = size
+        
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, new_brand):
+        if type(new_brand) is str:
+            self._brand = new_brand
+        else:
+            print("brand must be a string")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if type(new_size) is int:
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        '''says that the shoe has been repaired.'''
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+        return self.condition
+    
+   
+
+    
